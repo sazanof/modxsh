9908b8c2987f4de75dfc239ce65d02053e420084
Михаил <sazanof.ru@yandex.ru>
2017-01-06 10:13:31 +0300

    UPD 2

--- assets/plugins/modxsh/plugin.php
+++ assets/plugins/modxsh/plugin.php
@@ -35,30 +35,35 @@ if (!defined('MODX_BASE_PATH')) {
 }
 global $modx;
 $e = &$modx->Event;
+$output =  '';
 define("SH_DIR", MODX_SITE_URL . 'assets/plugins/modxsh/syntaxhighlighter/');
-if ($e->name == 'OnWebPageInit') {
-    $out = '';
-    $theme = isset($theme) ? $theme : 'Default';
-    $cssTheme = 'shTheme' . $theme . '.css';
-    $cssTheme = str_replace(' ', '', $cssTheme);
-    $all = '<script type="text/javascript">
-         SyntaxHighlighter.autoloader(
-            ["js","jscript","javascript","' . SH_DIR . 'scripts/shBrushJScript.js"],
-            ["bash","shell","' . SH_DIR . 'scripts/shBrushBash.js"],
-            ["css","' . SH_DIR . 'scripts/shBrushCss.js"],
-            ["xml","html","' . SH_DIR . 'scripts/shBrushXml.js"],
-            ["sql","' . SH_DIR . 'scripts/shBrushSql.js"],
-            ["php","' . SH_DIR . 'scripts/shBrushPhp.js"],
-            ["plain","' . SH_DIR . 'scripts/shBrushPlain.js"]
-         );
-         SyntaxHighlighter.config.stripBrs = true;
-         SyntaxHighlighter.all()
-    </script>';
-    $modx->regClientCSS(SH_DIR . 'styles/shCore.css');
-    $modx->regClientCSS(SH_DIR . 'fix.css');
-    $modx->regClientCSS(SH_DIR . 'styles/' . $cssTheme);
-    $modx->regClientScript(SH_DIR . 'scripts/shCore.js');
-    $modx->regClientScript(SH_DIR . 'scripts/shAutoloader.js');
-    $modx->regClientScript($all);
-    $e->output($out);
+switch ($e->name) {
+    case 'OnLoadWebDocument':
+        $f = array('<code>','</code>','language-php','language-html');
+        $r = array('','','brush: php','brush: html');
+        $modx->documentContent = str_replace($f,$r,$modx->documentContent);
+        //die;
+        $theme = isset($theme) ? $theme : 'Default';
+        $cssTheme = 'shTheme' . $theme . '.css';
+        $cssTheme = str_replace(' ', '', $cssTheme);
+        $all = '<script type="text/javascript">
+             SyntaxHighlighter.autoloader(
+                ["js","jscript","javascript","' . SH_DIR . 'scripts/shBrushJScript.js"],
+                ["bash","shell","' . SH_DIR . 'scripts/shBrushBash.js"],
+                ["css","' . SH_DIR . 'scripts/shBrushCss.js"],
+                ["xml","html","' . SH_DIR . 'scripts/shBrushXml.js"],
+                ["sql","' . SH_DIR . 'scripts/shBrushSql.js"],
+                ["php","' . SH_DIR . 'scripts/shBrushPhp.js"],
+                ["plain","' . SH_DIR . 'scripts/shBrushPlain.js"]
+             );
+             SyntaxHighlighter.config.stripBrs = true;
+             SyntaxHighlighter.all()
+        </script>';
+        $output .= $modx->regClientCSS(SH_DIR . 'styles/shCore.css');
+        $output .= $modx->regClientCSS(SH_DIR . 'fix.css');
+        $output .= $modx->regClientCSS(SH_DIR . 'styles/' . $cssTheme);
+        $output .= $modx->regClientScript(SH_DIR . 'scripts/shCore.js');
+        $output .= $modx->regClientScript(SH_DIR . 'scripts/shAutoloader.js');
+        $output .= $modx->regClientScript($all);
+        break;
 }
